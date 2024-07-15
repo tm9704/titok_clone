585b6ef595ddb45cf639b88ea8840ae94fdefac0
tm9704 <tm9704@korea.ac.kr>
2024-07-15 15:43:59 +0900

    Set Constants

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:titok_clone/constants/gaps.dart';
+import 'package:titok_clone/constants/sizes.dart';
 
 void main() {
   runApp(const TitokApp());
@@ -15,7 +17,18 @@ class TitokApp extends StatelessWidget {
       theme: ThemeData(
         primaryColor: const Color(0xFFE9435A),
       ),
-      home: Container(),
+      home: Padding(
+        padding: const EdgeInsets.all(Sizes.size14),
+        child: Container(
+          child: const Row(
+            children: [
+              Text('hello'),
+              Gaps.h20,
+              Text('hello'),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
