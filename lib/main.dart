d912e4cc5038cd88832dd8ae6edcc543ec95f312
tm9704 <tm9704@korea.ac.kr>
2024-07-16 15:07:34 +0900

    UI for Sign Up Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:titok_clone/constants/gaps.dart';
 import 'package:titok_clone/constants/sizes.dart';
+import 'package:titok_clone/screens/sign_up/sign_up_screen.dart';
 
 void main() {
   runApp(const TitokApp());
@@ -17,18 +18,7 @@ class TitokApp extends StatelessWidget {
       theme: ThemeData(
         primaryColor: const Color(0xFFE9435A),
       ),
-      home: Padding(
-        padding: const EdgeInsets.all(Sizes.size14),
-        child: Container(
-          child: const Row(
-            children: [
-              Text('hello'),
-              Gaps.h20,
-              Text('hello'),
-            ],
-          ),
-        ),
-      ),
+      home: const SignUpScreen(),
     );
   }
 }
