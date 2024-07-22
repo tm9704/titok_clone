79853f6aeb2e7d419be18c4e14d3d2eb42d76a0c
tm9704 <tm9704@korea.ac.kr>
2024-07-22 17:48:46 +0900

    Username Screen

--- lib/features/authentication/sign_up_screen.dart
+++ lib/features/authentication/sign_up_screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:titok_clone/constants/gaps.dart';
 import 'package:titok_clone/constants/sizes.dart';
-import 'package:titok_clone/features/authentication/email_screen.dart';
+import 'package:titok_clone/features/authentication/username_screen.dart';
 import 'package:titok_clone/features/authentication/login_screen.dart';
 import 'package:titok_clone/features/authentication/widgets/auth_button.dart';
 
@@ -20,7 +20,7 @@ class SignUpScreen extends StatelessWidget {
   void _onEmailTap(BuildContext context) {
     Navigator.of(context).push(
       MaterialPageRoute(
-        builder: (context) => const EmailScreen(),
+        builder: (context) => const UsernameScreen(),
       ),
     );
   }
